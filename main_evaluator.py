bdf81e13a4200c83e775672db2159bf3bdb5aacf
Vladimir Usatikov <vladimir@usatikov.com>
2020-05-06 00:36:41 +0200

    ___

--- main_evaluator.py
+++ main_evaluator.py
@@ -51,9 +51,10 @@ class Evaluator:
         # you need to set blit=False, or the first set of arrows never gets
         # cleared on subsequent frames
         anim = animation.FuncAnimation(fig, update_quiver, fargs=(Q, ),
-                                       interval=50, blit=False)
-        #anim.save("test.gif", fps=60)
+                                       interval=5, frames=1000, blit=False)
+
         plt.show()
+        # anim.save('animation.mp4', writer=animation.FFMpegWriter())
 
     def psi_feld_animation(self):
         self.restart()
@@ -84,72 +85,6 @@ class Evaluator:
 
         anim = animation.FuncAnimation(fig, animate, fargs=(cont, ), repeat=False)
         # anim.save('animation.mp4', writer=animation.FFMpegWriter())
-
-        plt.show()
-
-    def omega_feld_animation(self):
-        self.restart()
-        t, psi, vx, vy = next(self._iterator)
-
-        X = np.arange(0, self.solver.L + self.solver.h, self.solver.h)
-        Y = np.arange(0, self.solver.d + self.solver.h, self.solver.h)
-
-        fig = plt.figure()
-        ax = plt.axes(xlabel='x', ylabel='y')
-
-        levels = MaxNLocator(nbins=50).tick_values(self.solver.omega.min(), self.solver.omega.max())
-        cont = plt.contourf(X, Y, self.solver.omega, levels = levels)
-        cbar = plt.colorbar()
-
-        min_max_text = ax.text(0.51, 0.01, '', transform=ax.transAxes)
-        time_text = ax.text(0.01, 0.01, '', transform=ax.transAxes)
-
-        # animation function
-        def animate(i, cont):
-            t, psi, vx, vy = next(self._iterator)
-            ax.collections = []
-            levels = MaxNLocator(nbins=50).tick_values(self.solver.omega.min(), self.solver.omega.max())
-
-            # cbar.ScalarMappable.set_clim((self.solver.omega.min(), self.solver.omega.max()))
-            cont = plt.contourf(X, Y, self.solver.omega, levels = levels)
-            time_text.set_text('Zeit = %.1f s' % t)
-            min_max_text.set_text('Min = %.1f, Max = %.1f' % (self.solver.omega.min(), self.solver.omega.max()))
-            return cont, time_text, min_max_text
-
-        anim = animation.FuncAnimation(fig, animate, fargs=(cont, ), repeat=False)
-        # anim.save('animation.mp4', writer=animation.FFMpegWriter())
-
-        plt.show()
-
-    def psi_feld_animation(self):
-        self.restart()
-        t, psi, vx, vy = next(self._iterator)
-
-        X = np.arange(0, self.solver.L + self.solver.h, self.solver.h)
-        Y = np.arange(0, self.solver.d + self.solver.h, self.solver.h)
-
-        fig = plt.figure()
-        ax = plt.axes(xlabel='x', ylabel='y')
-
-        levels = MaxNLocator(nbins=50).tick_values(psi.min(), psi.max())
-        cont = plt.contourf(X, Y, psi, levels = levels)
-        cbar = plt.colorbar()
-
-        time_text = ax.text(0.01, 0.01, '', transform=ax.transAxes)
-
-        # animation function
-        def animate(i, cont):
-            t, psi, vx, vy = next(self._iterator)
-            ax.collections = []
-            # levels = MaxNLocator(nbins=50).tick_values(psi.min(), psi.max())
-            # cont.mappable.set_clim(psi.min(), psi.max())
-            # cbar.draw_all()
-            cont = plt.contourf(X, Y, psi, levels = levels)
-            time_text.set_text('Zeit = %.1f s' % t)
-            return cont, time_text
-
-        anim = animation.FuncAnimation(fig, animate, fargs=(cont, ), repeat=False)
-        anim.save('animation.mp4', writer=animation.FFMpegWriter())
 
         plt.show()
 
@@ -196,32 +131,9 @@ if __name__ == "__main__":
     # evaluator = Evaluator(solver)
     # evaluator.psi_feld_animation()
 
-
     solver = IterSolver(RukuTimeSolver(), ny=21, L=1, d=1, dt=0.01, V_in=0)
     solver.set_omega0_VB()
-
-    from matrix_solver import MatrixSolver, EulerTimeSolver
-    solver = MatrixSolver(EulerTimeSolver(), ny=21, x=1, y=1, t_max=100, dt=.1, algorithmus="JAC")
-    #solver.set_omega0()
-
     evaluator = Evaluator(solver)
-    #evaluator.psi_feld_animation()
     evaluator.v_feld_animation()
-
     # evaluator.psi_feld_animation()
     # evaluator.omega_feld_animation()
-
-
-    """
-    from iter_solver import IterSolver, RukuTimeSolver
-    solver = IterSolver(RukuTimeSolver(), ny=21, L=1, d=1, V_in=0)
-    solver.set_omega0_VB()
-    evaluator = Evaluator(solver)
-    evaluator.v_feld_animation()
-    # evaluator.psi_feld_animation()
-
-    solver = IterSolver(RukuTimeSolver(), ny=21, L=1, d=1, V_in=0)
-    solver.set_omega0_VB()
-    evaluator = Evaluator(solver)
-    evaluator.psi_feld_animation() 
-    """
